5cca16257aa67a3edab824a99b43c20062d969e7
Aaron Fowlkes <anbfowlkes@gmail.com>
2022-06-11 18:14:20 -0400

    commit tree 50bb09f8e6092c268f0c29687204cce1b2121f55

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Write your solution in this file!
+const employee = {
+    name: "Aaron Fowlkes",
+    streetAddress: "21 Gladbrook Rd"
+}
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    let a = {...employee};
+    a[key] = value;
+    return a;
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+function deleteFromEmployeeByKey(employee, key) {
+    let b = {...employee};
+    delete b[key];
+    return b;
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+}
